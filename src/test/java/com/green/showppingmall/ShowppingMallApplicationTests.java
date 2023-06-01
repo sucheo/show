92bf5e5a07585579d61c5dea720381ec24887ed1
성수천 <3598ysm@gmail.com>
2023-06-01 19:05:45 +0900

    aa

---
+++ src/test/java/com/green/showppingmall/ShowppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.showppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
